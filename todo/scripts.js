ef70fc8c716185b3d9f774744c8299e0830ff96d
Pierre Nguyen <pierrenguyen@Pierres-MacBook-Pro.local>
2020-06-15 15:06:28 -0700

    bugfix todo

--- todo/scripts.js
+++ todo/scripts.js
@@ -10,7 +10,7 @@ const UNCHECK = "fa-circle-thin";
 const LINE_THROUGH = "lineThrough";
 
 // Variables
-let LIST, id;
+let LIST,id;
 
 // Get items from localStorage
 let data = localStorage.getItem('TODO');
@@ -41,7 +41,8 @@ const today = new Date();
 
 dateElement.innerHTML = today.toLocaleDateString('en-US', options);
 
-function addToDo(toDo, id, done ,trash) {
+function addToDo(toDo, id, done, trash){
+  if(trash){ return; }
   const DONE = done ? CHECK : UNCHECK;
   const LINE = done ? LINE_THROUGH : "";
   const item = `<li class="item">
@@ -49,48 +50,49 @@ function addToDo(toDo, id, done ,trash) {
                   <p class="text ${LINE}">${toDo}</p>
                   <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
               </li>`
-  const position = "beforeend"
-  list.insertAdjacentElement(position, item);
+  const position = "beforeend";
+  list.insertAdjacentHTML(position, item);
 }
 
 
-document.addEventListener("keyup", function(event){
-if(event.keyCode == 13){
-  const toDo = input.nodeValue;
-  if(toDo){
-    addToDo(toDo, id, false, false)
-      LIST.push({
-        name : toDo,
-        id : id,
-        done : false,
-        trash : false
-      });
-      localStorage.setItem("TODO",JSON.stringify(LIST));
+document.addEventListener("keyup",function(even){
+  if(event.keyCode == 13){
+    const toDo = input.value;
+    // if the input isn't empty
+    if(toDo){
+      addToDo(toDo, id, false, false);
+        LIST.push({
+                name : toDo,
+                id : id,
+                done : false,
+                trash : false
+        });
+        // add item to localStorage. MUST BE ADDED WHERE LIST ARRAY IS UPDATED.
+        localStorage.setItem("TODO", JSON.stringify(LIST));
         id++;
+    }
+    input.value = "";
   }
-  input.value = "";
-}
 });
 
 function completeToDo(element){
   element.classList.toggle(CHECK);
   element.classList.toggle(UNCHECK);
-  element.parentNode.querySelector('.text').classList.toggle(LINE_THROUGH);
-  LIST[element.id].trash = true;
-};
+  element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+  LIST[element.id].done = LIST[element.id].done ? false : true;
+}
 
 function removeToDo(element){
   element.parentNode.parentNode.removeChild(element.parentNode);
   LIST[element.id].trash = true;
-};
+}
 
-list.addEventListener("click", function(event){
-  const element = event.target;
-  const elementJob = element;
-
-  if(elementJob == "complete"){
-    completeToDo(element);
-  } elseif (elementJob == "delete"){
-    removeToDo(element);
+list.addEventListener("click", function(event) {
+  const element = event.target; // return the clicked element inside list
+  const elementJob = element.attributes.job.value; // complete or delete
+  if(elementJob == "complete") {
+    completeToDo(element)
+  }else if(elementJob == "delete"){
+    removeToDo(element)
   }
-})
+});
